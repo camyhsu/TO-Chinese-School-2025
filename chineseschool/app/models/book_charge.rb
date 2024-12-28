ed189612a79cf94c4a4d1bb68b598494c417f3a4
CHIAMING HSU <camy@MacBookAir.attlocal.net>
2024-12-27 20:37:33 -0800

    Add base model BookCharge

---
+++ chineseschool/app/models/book_charge.rb
@@ -0,0 +1,3 @@
+class BookCharge < ApplicationRecord
+  validates :book_charge_in_cents, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+end
---
+++ chineseschool/test/models/book_charge_test.rb
@@ -0,0 +1,31 @@
+require "test_helper"
+
+class BookChargeTest < ActiveSupport::TestCase
+  test "valid book charge can be saved" do
+    valid_book_charge = random_book_charge
+    assert valid_book_charge.save
+  end
+
+  test "invalid if no book charge in cents" do
+    book_charge_without_value = random_book_charge
+    book_charge_without_value.book_charge_in_cents = nil
+    assert_not book_charge_without_value.valid?
+  end
+
+  test "invalid if no book charge in cents is not an integer" do
+    non_integer_book_charge = random_book_charge
+    non_integer_book_charge.book_charge_in_cents = SecureRandom.random_number
+    assert_not non_integer_book_charge.valid?
+  end
+
+  test "invalid if book charge in cents is negative" do
+    negative_book_charge = random_book_charge
+    negative_book_charge.book_charge_in_cents = - rand(100000)
+    assert_not negative_book_charge.valid?
+  end
+
+  private
+  def random_book_charge
+    BookCharge.new(grade_id: rand(1..10), school_year_id: rand(1..20), book_charge_in_cents: rand(100000))
+  end
+end
